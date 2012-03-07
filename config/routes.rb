65cda5576cb8c4093b45cc334ca901a8e89b9eb4
Alberto Vena <kennyadsl@gmail.com>
2012-03-07 09:39:17 +0100

    Do not allow get requests to confirm payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   post "paypal/confirm", :to => "paypal#confirm", :method => :post
-  get "paypal/confirm", :to => "paypal#confirm", :method => :get
-
   resources :payment_notifications, :only => [:create]
 end
